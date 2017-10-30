11e227f467b9af57dab8030c1ab7f30bd6493611
julian <julian.mauborgne@progdis.com>
2017-10-30 15:49:16 +0100

    nouveau menu slide

--- view/js/fonc_aff.js
+++ view/js/fonc_aff.js
@@ -1,12 +1,48 @@
 $(function($){
-	$('#menuParameters').click(function(){
-		$('.sidepanel').toggleClass('visible');
- 	});
 
-	$('.menuPanelHide').click(function() {
-		if($('.sidepanel').hasClass('visible')) {
-			$('.sidepanel').removeClass('visible');
+
+	(function() {
+		var panels = 'sidepanel';
+		$('#menuParameters').click(function(){
+			var pan = 'parameters';
+			togglePanel(pan, panels);
+	 	});
+
+		$('#menuRoyaume').click(function(){
+			var pan = 'royaume';
+			togglePanel(pan, panels);
+	 	});
+
+		$('#menuProfil').click(function(){
+			var pan = 'profil';
+			togglePanel(pan, panels);
+	 	});
+	})();
+
+	/////
+
+	function togglePanel(panel, classToClose) {
+		$('.' + panel).toggleClass('visible');
+		fermerPanels(panel, classToClose);
+	}
+
+	function fermerPanels(src, classToClose) {
+		var divs = $('.' + classToClose);
+
+		for(var i = 0; i < divs.length; i++) {
+			var div = divs[i];
+
+			if(!($(div).hasClass(src))) {
+				fermerPanel(div);
+			}
+
 		}
-	})
+	}
+	
+	function fermerPanel(panel) {
+		if($(panel).hasClass('visible')) {
+			$(panel).removeClass('visible');
+		}
+	}
 
 });
